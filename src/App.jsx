976c51ea0a0b41a91ec848ed2b835fec8dde5e12
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-22 18:37:12 +0100

    create folders for handling and orgnizing the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 /* eslint-disable react/react-in-jsx-scope */
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
 function App() {
-  return <div>sofiane</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div style={{ margin: '10px' }}>
+        <Button size="small" variation="primary">
+          Click me
+        </Button>
+
+        <Button size="medium" variation="secondary">
+          Click me
+        </Button>
+
+        <Button size="medium" variation="danger">
+          delete
+        </Button>
+      </div>
+    </>
+  );
 }
 
 export default App;

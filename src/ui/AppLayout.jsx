68b37be042958d163aa7eaf19733edd279c7ebad
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-22 22:58:17 +0100

    update some sidebare style in Applayout component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,35 +1,33 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
+import SideBar from './SideBar';
 
-const SideBar = styled.aside`
-  background-color: red;
-  grid-row: 1/-1;
-`;
-
-const NavBar = styled.nav`
-  background-color: blue;
+const Header = styled.nav`
+  background-color: var(--color-grey-0);
+  border-block-end: 1px solid var(--color-grey-100);
+  padding: 0.8rem 0.4rem;
 `;
 
 const Main = styled.main`
-  background-color: orange;
+  background-color: var(--color-grey-50);
 `;
 
 const StyledAppLayout = styled.div`
   height: 100%;
   display: grid;
-  grid-template-columns: 25rem 1fr;
+  grid-template-columns: 18rem 1fr;
   grid-template-rows: auto 1fr;
 `;
 
 function AppLayout() {
   return (
     <StyledAppLayout>
-      <NavBar>NavBar</NavBar>
+      <Header>Header</Header>
       <Main>
         <Outlet />
       </Main>
-      <SideBar>SideBar</SideBar>
+      <SideBar />
     </StyledAppLayout>
   );
 }

4eb1428a81f00930e94d74dad7df58de3df840b9
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-22 16:39:42 +0100

    finish setup the initial enveronment

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'import'],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Not needed in Vite
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/react-in-jsx-scope */
+function App() {
+  return <div>sofiane</div>;
+}
+
+export default App;
